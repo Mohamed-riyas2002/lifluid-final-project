0164a05416cd65c85661be91d70f80273455f3b2
Mohamed-riyas2002 <rasoolmohamed669@gmail.com>
2022-07-25 12:09:00 +0530

    added project

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  get 'donors/donor_register'
+  get 'users/index'
+  match 'login',to:'users#login', via: :get
+  match 'users/register', to: 'users#register', via: :get
+  match 'users/create', to: 'users#create', via: :post
+  match 'users/validate', to: 'users#validate', via: :post
+  match 'users/destroy', to: 'users#destroy', via: :get
+  root 'users#index'
+
+
+
+
+
+
+
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
